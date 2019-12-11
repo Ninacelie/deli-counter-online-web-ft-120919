ea264a614d235b32442d7f75fd267b83dedd1fdd
Nina Angelo <ninacelie@gmail.com>
2019-12-11 16:56:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+katz_deli = [0]
+
+def line(array)
+  if array.count == 0
+    puts "The line is currently empty."
+  else
+    response "The line is currently:"
+    array.each_with_index do |name, index|
+      response += "#{index+1}.#{name}"
+  end
+  puts response
+end
+end
